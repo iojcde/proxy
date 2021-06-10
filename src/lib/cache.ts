24a0a6bc4808da61140a5c70fdee783c08092e30
JcdeA <jcde@jcde.xyz>
2021-06-10 18:22:16 +0900

    fix: don't clean up default node types

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -5,9 +5,6 @@ const options = {
   length: function (n, key) {
     return n * 2 + key.length
   },
-  dispose: function (key, n) {
-    n.close()
-  },
   maxAge: 1000 * 60 * 60,
 }
 const cache = new LRU(options)
