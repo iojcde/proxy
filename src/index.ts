d5e9641a3c438cb1fc91abe370c4d6d70cd858d8
JcdeA <jcde@jcde.xyz>
2021-06-12 17:13:06 +0900

    feat: use plain http2..

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { SecureContext } from 'tls'
-import https from 'https'
+import http2 from 'http2'
 import http from 'http'
 import { getSecureContext } from './lib/ssl'
 import dotenv from 'dotenv'
 import { parseConfig, getUrl } from './lib/config'
-import Restana from 'restana'
-import { proxy } from 'fast-proxy'
+import h2request from 'http2-wrapper'
+import proxy from 'http2-proxy'
+
 const options = {
+  allowHTTP1: true,
   // A function that will be called if the client supports SNI TLS extension(Pretty much every modern browser).
   SNICallback: async (servername: string, cb: (err: Error, ctx: SecureContext | null) => void) => {
     const ctx = await getSecureContext(servername)
@@ -18,17 +20,54 @@ const options = {
   },
 }
 
-const restana = Restana({
-  server: https.createServer(options),
-})
+const defaultWebHandler = (
+  err: Error,
+  req: http2.Http2ServerRequest,
+  res: http2.Http2ServerResponse,
+): void => {
+  if (err) {
+    console.error('proxy error', err)
+    res.end('Internal server error')
+  }
+}
 
-restana.all('/*', function (req, res) {
+const defaultWSHandler = (err: Error, req: http2.Http2ServerRequest, socket): void => {
+  if (err) {
+    console.error('proxy error', err)
+    socket.destroy()
+  }
+}
+const server = http2.createSecureServer(options)
+
+server.on('request', (req, res) => {
   const url = getUrl(req.headers.host, config)
   console.log('url: ' + url)
-  proxy(req, res, req.url, { base: `http://${url.domain}:${url.port}` })
+  proxy.web(
+    req,
+    res,
+    {
+      hostname: url.domain,
+      port: url.port,
+      onReq: async (req, options) => h2request.request(options),
+    },
+    defaultWebHandler,
+  )
 })
-restana.start(443)
 
+server.on('upgrade', (req, res, head) => {
+  const url = getUrl(req.headers.host, config)
+  console.log('url: ' + url)
+  proxy.ws(
+    req,
+    res,
+    head,
+    {
+      hostname: url.domain,
+      port: url.port,
+    },
+    defaultWSHandler,
+  )
+})
 dotenv.config()
 
 const config = parseConfig()
